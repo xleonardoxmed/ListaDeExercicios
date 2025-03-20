4ad0f2d9bf73960d6a372dd15bc50cf3e841a91a
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-20 14:49:45 -0300

    Ex04

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Exercicio04
+{
+    internal class Program
+    { // Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("Conversor de temperatura C -> F");
+                Console.WriteLine("-----------------------------------------");
+                Console.Write("Digite a temperatura na escala Celsius: ");
+                Console.WriteLine("\n-----------------------------------------");
+                decimal Ctemperature = Convert.ToDecimal(Console.ReadLine());
+                decimal Ftemperature = (Ctemperature * 1.8m) + 32;
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine($"A temperatura na escala Fahrenheit fica: {Ftemperature} ");
+  
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("Gostaria de fazer uma nova conversão? (S/N)");
+                Console.WriteLine("-----------------------------------------");
+                string toContinue = Console.ReadLine()!.ToUpper();
+                if (toContinue != "S")
+                    break;
+            }
+           
+            
+
+        }
+    }
+}
