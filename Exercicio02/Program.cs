5e172632d0fa28e00162d300afe934229afb60e2
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-20 14:14:13 -0300

    Ex02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double radius = 3.1;
+            double height = 10;
+            double volume = Math.PI * Math.Pow(radius, 2) * height;
+
+            Console.WriteLine($"O volume do cilindro é: {volume} cm³");
+            Console.ReadLine();
+        }
+    }
+}
