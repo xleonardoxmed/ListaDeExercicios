3b903d4d5ad8991280231af58e50ce9a8ba73932
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-20 13:56:22 -0300

    Ex01

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o comprimento da caixa: ");
+            decimal length = Convert.ToDecimal(Console.ReadLine());
+            
+            Console.WriteLine("Digite a largura da caixa: ");
+            decimal width = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura da caixa: ");
+            decimal height = Convert.ToDecimal(Console.ReadLine());
+
+            decimal result = length + width + height;
+            Console.ReadLine()
+        }
+    }
+}
