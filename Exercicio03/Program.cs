dd6e411fecdc6e81787fac725d22c4c4e2db7f9d
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-20 14:27:02 -0300

    Ex03

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.Write("Digite a quilometragem inicial: ");
+            double kmBefore = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------------------");
+            Console.Write("Digite a quilometragem final: ");
+            double kmAfter = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------------------");
+            Console.Write("Digite o gasto de combustível: ");
+            double fuelSpend = Convert.ToDouble(Console.ReadLine());
+
+            double route = kmAfter - kmBefore;
+            double fuelPerKmSpend = route / fuelSpend;
+
+            Console.WriteLine($"O consumo de combustível durante o percurso foi de ~ {fuelPerKmSpend} Km/L");
+            Console.ReadLine();
+        }
+    }
+}
