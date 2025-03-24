f12d9e1c64c2545f5a8936e9d1a7ff8f1c0b2287
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-23 23:49:42 -0300

    Ex19

---
+++ Exercicio19/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Enumerable.Range(1, 500).ToArray(); // intervalo
+            int[] oddNumbers = numbers.Where(n => n % 2 != 0).ToArray(); // ímpares
+            int[] multiplesOf3 = oddNumbers.Where(n => n % 3 == 0).ToArray(); // Múltiplos de três
+            int sum = multiplesOf3.Sum(); // Soma dos elementos
+            Console.WriteLine("------");
+            Console.WriteLine(sum); // impressão
+            Console.WriteLine("------");
+            Console.ReadLine();
+        }
+    }
+}
